37d70c4d06fdd8464991a4444d7d9ab85f26262f
dericagilroy <dericagilroy24@gmail.com>
2021-11-09 23:45:27 -0500

    Edited world.js; added an event listener to the "Lookup Cities" button that displays information about the cities in the country entered

--- world.js
+++ world.js
@@ -4,7 +4,17 @@ window.addEventListener("DOMContentLoaded", function() {search()});
 
 function search() {
     document.getElementById("lookup").addEventListener("click", function() {
-        fetch("world.php?country=" + $("#country").val())
+        fetch("world.php?country=" + $("#country").val() + "&context=")
+            .then(response => {
+                return response.text();
+            })
+            .then(data => {
+                document.getElementById("result").innerHTML = data;
+            })
+    });
+
+    document.getElementById("lookup_cities").addEventListener("click", function() {
+        fetch("world.php?country=" + $("#country").val() + "&context=cities")
             .then(response => {
                 return response.text();
             })
